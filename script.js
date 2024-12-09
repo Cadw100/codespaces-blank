338a0de219d8243e5e21b21c62f447c75f65c65c
Cadw100 <crwarren1@cps.edu>
2024-12-09 17:20:16 +0000

    FINAL!

--- script.js
+++ script.js
@@ -8,11 +8,13 @@ let injury = document.querySelector('.injury');
 let monkey = document.querySelector('.monkey');
 let gs = document.querySelector('.gs');
 let oblock = document.querySelector('.oblock');
+let monkeyE = document.querySelector('.monkeyE');
 shotgun.style.display="none"; 
 injury.style.display="none";
 gs.style.display="none";
 oblock.style.display="none";
 monkey.style.display="none";
+
 
 
 
@@ -24,6 +26,8 @@ left.addEventListener('click', function() {
  left.style.display ="none";
 right.style.display ="none";
 shotgun.style.display="block";
+monkey.style.display="none";
+gs.style.display="none";
 text.innerHTML="Double click to shoot";
 
 });
@@ -35,7 +39,7 @@ shotgun.addEventListener('dblclick', function(){
     shotgun.style.display="none";
    injury.style.display= "none";
    gs.style.display= "none";
-   gs.style.display= "block";
+
    injury.style.display="block";
    text.innerHTML ="OUCH";
    text.innerHTML="You shot a random for 0 reason"; 
@@ -51,19 +55,38 @@ shotgun.addEventListener('dblclick', function(){
         console.log("right clicked")
         leftRight.style.display ="none";
         left.style.display ="none";
+        injury.style.display="none";
         right.style.display ="none";
         gs.style.display= "none";
         gs.style.display = "block";
-        shotgun.style.display ="block";
+        shotgun.style.display ="none";
         text.innerHTML ="You begin at a gun store";
 
 });
 
+shotgun.addEventListener('dbl', function()){
+
+    continue here
+ 
+
+
 // mouse enter event
 
-monkey.addEventListener('mouseenter', function() { 
+monkeyE.addEventListener('mouseenter', function() { 
+    console.log("monkeyE");
     shotgun.style.display="none";
+    injury.style.display="none";
+    monkey.style.display="block";
+    gs.style.display="none";
 
-    text.innerHTML= "Heres a medkit lil bro";
+     text.innerHTML= "Heres a medkit lil bro";
     
 });
+
+monkey.addEventListener('dblclick', function(){
+    console.log("monkey dbl clicked");
+    COntinute here
+
+
+});
+
